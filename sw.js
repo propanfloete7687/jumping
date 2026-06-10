1bd71f4bff3be4745f97fc5f432b63f2f46dcc61
propanfloete7687 <ingoburgold87@gmail.com>
2026-06-10 08:26:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,8 @@
 const CACHE = 'jumping-fitness-v1';
-const FILES = ['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+const FILES = [
+  '/jumping/',
+  '/jumping/index.html',
+  '/jumping/manifest.json',
+  '/jumping/icon-192.png',
+  '/jumping/icon-512.png'
+];
